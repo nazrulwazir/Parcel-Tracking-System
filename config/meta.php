43dd0bb2ad37e4b95bf68833405bee88bdaf9143
nazrulwazir <nazrulhakimwazir@gmail.com>
2018-05-04 00:23:46 +0800

    update meta

--- config/meta.php
+++ config/meta.php
@@ -2,7 +2,7 @@
 
 return [
     'author'      => env('META_AUTHOR', env('APP_URL')),
-    'keywords'    => env('META_KEYWORDS', "Pos Laju Tracking System , Pos Laju , Parcel , Nazrul Wazir"),
-    'description' => env('META_DESCRIPTION', 'Pos Laju Tracking System'),
+    'keywords'    => env('META_KEYWORDS', "Parcel Tracking System , Pos Laju , Gdex, SkyNet Express,DHL Express,ABX Express, Parcel , Nazrul Wazir"),
+    'description' => env('META_DESCRIPTION', 'Parcel Tracking System'),
     'title'       => env('META_TITLE', env('APP_URL')),
 ];
