177d39ae6af815f691f30b1ff06322fd184dbd4e
nazrulwazir <nazrulhakimwazir@gmail.com>
2018-05-05 17:58:40 +0800

    add more option

--- resources/views/components/history.blade.php
+++ resources/views/components/history.blade.php
@@ -5,12 +5,12 @@
 	@foreach($list_session as $value)
 	<div class="col-md-4">
 	<div class="alert alert-info text-center ">
-		<a href="{{ route('manage.track', [$value->parcel_type,$value->tracking_num]) }}">
+		<a href="{{ route('manage.track', [$value->parcel_type,$value->tracking_num]) }}" style="text-decoration:none">
 			<div class="container-fluid">
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				<span aria-hidden="true"><i class="material-icons">clear</i></span>
 				</button>
-				<span aria-hidden="true"><img src="{{ asset('assets/img/'.$value->parcel_type.'.png') }}" width="90"></span><br/>
+				<span aria-hidden="true"><div style="vertical-align: top;"> <img src="{{ asset('assets/img/'.$value->parcel_type.'.png') }}" style=" border: 2px solid #eee; border-radius: 5px; height: 95px;"> </div></span><br/>
 				{{ $value->tracking_num }}<br/> <b>{{ $value->info }}</b>
 			</div></a>
 		</div></div>
--- resources/views/components/show_data.blade.php
+++ resources/views/components/show_data.blade.php
@@ -1,19 +1,58 @@
+@push('scripts')
+<script src="{{ asset('js/share.js') }}"></script>
+<script type="text/javascript">
+	function copyToClipboard(element) {
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val($(element).text()).select();
+	document.execCommand("copy");
+	$temp.remove();
+	}
+	$(".btnCopy").click(function(){
+	    swal(
+		  'Success',
+		  'Copied to clipboard',
+		  'success'
+		)
+	});
+</script>
+@endpush
+@push('styles')
 <style type="text/css">
 	.card .header-primary {
 	background: linear-gradient(60deg, #6cd1ff, #03a9f4);
 	}
+	.dropdown-menu li a:hover, .dropdown-menu li a:focus, .dropdown-menu li a:active {
+	background-color: #03a9f4;
+	color: #FFFFFF;
+	}
 </style>
+@endpush
 <div class="col-md-8 col-md-offset-2">
 	<br/><br/><br/>
 	<div class="card card-signup">
 		
 		<div class="header header-primary text-center">
-			<div class="logo">
-				<img src="{{ asset('assets/img/'.$parcel_type.'.png') }}" width="200">
-			</div>
-			<h4><b>{{ __('wording.track_number') }}</b></h4>
+			
+			<div style="vertical-align: top;"> <img src="{{ asset('assets/img/'.$parcel_type.'.png') }}" style=" border: 2px solid #eee; border-radius: 5px; height: 95px;"> </div>
 			<h4><b>{{ $tracking_num }} </b></h4>
 			<h4><i class="fas fa-truck"></i>  {{ $title[0]['process'] or 'Record Not Found'}}</h4>
+			<div>
+				<button class="btn btn-warning btn-sm btnSendEmail" title="" data-toggle="tooltip" data-original-title="Send Email To Receiver"> <i class="fas fa-bell"></i> Notification</button>
+				<button class="btn btn-warning btn-sm btnCopy" title="" data-toggle="tooltip" data-original-title="Copy URL" onclick="copyToClipboard('#copy-text')"> <i class="fas fa-link"></i> Copy </button>
+				<div id="copy-text" hidden>{{ route('manage.track', [$parcel_type,$tracking_num]) }}</div>
+				<button class="btn btn-warning btn-sm" title="" data-toggle="tooltip" data-original-title="Save or Print"> <i class="fas fa-print"></i> Print </button>
+				<div class="btn-group" >
+					<button type="button" class="btn btn-warning dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+					<i class="fas fa-share-alt"></i> Share <span class="caret"></span>
+					</button>
+					<ul class="dropdown-menu dropdown-menu-right">
+						<li><a href="https://www.facebook.com/sharer/sharer.php?u={{ route('manage.track', [$parcel_type,$tracking_num]) }}" class="social-button "><i class="fab fa-facebook"></i> Facebook </a></li>
+						<li><a href="https://twitter.com/intent/tweet?text=Shipment Status Tracking Result&amp;url={{ route('manage.track', [$parcel_type,$tracking_num]) }}" class="social-button "><i class="fab fa-twitter"></i> Twitter </a></li>
+						<li class="hidden-sm hidden-lg hidden-md"><a href="whatsapp://send?text={{ route('manage.track', [$parcel_type,$tracking_num]) }}"><i class="fab fa-whatsapp"></i> WhatsApp </a></li>
+					</ul>
+				</div>
+			</div>
 		</div>
 		<div class=" col-md-8 col-md-offset-2">
 			
